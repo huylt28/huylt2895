54325060fd83145bf387969f101215f713c61719
pig pu <huyle2895@gmail.com>
2022-11-01 20:29:03 +0700

    asd

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const arr1=[
     {id:3,item:'pen',type:'stuff'},
     {id:4,item:'phone',type:'stuff'}]
 
-let arr2 = arr1.filter(function(item){
-   return item.type == 'stuff';
-});
+// let arr2 = arr1.filter(function(item){
+//    return item.type == 'stuff';
+// });
 
- arr2 = arr2.map((item)=>{
-   return `<li id=${item.id} type=${item.type}> ${item.item} </li>`;
-});
+//  arr2 = arr2.map((item)=>{
+//    return `<li id=${item.id} type=${item.type}> ${item.item} </li>`;
+// });
 
-console.log(arr2);
+// console.log(arr2);
+
+function filter(arr1,string){
+  const result = arr1.filter(item => item.type === string)
+  return result.map(myFunction)
+}
+
+function myFunction(item){
+ return `<li id=${item.id} type=${item.type}> ${item.item} </li>`;
+}
+
+console.log(filter(arr1,'stuff'));
